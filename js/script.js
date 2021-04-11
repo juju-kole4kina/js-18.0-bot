86c624b7fe61743dc4176b611e467730bf46385d
Juliya Koltsova <juliyaasking009@gmail.com>
2021-04-11 03:26:43 +0200

    lesson06-hard done

--- js/script.js
+++ js/script.js
@@ -13,33 +13,46 @@ let getRandomInt = function (max) {
 
 let start = function () {
    let randomNum = getRandomInt(100);
+   let attempt = 10;
    let game = function () {
-      let num = prompt('Угадай число от 1 до 100');
-      if (num === null) {
-         alert('До свидания!');
-         return;
-      }
-      if (isNum(num)) {
-         let realNum = +num;
-         if (realNum > randomNum) {
-            alert('Загаданное число меньше');
-            game();
-         } else if (realNum < randomNum) {
-            alert('Загаданное число больше');
-            game();
+      attempt--;
+      if (attempt < 0) {
+         if (confirm('Поптки кончались, хотите сыграть ещё?')) {
+            start();
          } else {
-            if (confirm('Вы угадали! Сыграем ещё?')) {
-               start();
-            } else {
-               alert('До свидания!');
-               return;
-            }
+            alert('До свидания!');
+            return;
          }
       } else {
-         alert('Введите число');
-         game();
+         let num = prompt('Угадай число от 1 до 100');
+         if (num === null) {
+            alert('До свидания!');
+            return;
+         }
+         if (isNum(num)) {
+            let realNum = +num;
+            if (realNum > randomNum) {
+               alert(`Загаданное число меньше. У Вас осталось ${attempt} попыток`);
+               game();
+            } else if (realNum < randomNum) {
+               alert(`Загаданное число больше. У Вас осталось ${attempt} попыток`);
+               game();
+            } else {
+               if (confirm('Вы угадали! Сыграем ещё?')) {
+                  start();
+               } else {
+                  alert('До свидания!');
+                  return;
+               }
+            }
+         } else {
+            alert('Введите число');
+            game();
+         }
       }
    };
+
+
    game();
 };
 
